084e89268914d2da470d73eb2ee28fa2be7fa0da
Darkmatter865 <darkmatter865@gmail.com>
2017-10-17 20:41:37 +0530

    animations added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('animation1', [
+      
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+
+      state('large', style({
+        transform: 'scale(1.1)'
+      })),
+
+      transition('small => large', animate('300ms ease-in')),
+
+    ]),
+
+    trigger('flyInOut', [
+    
+    transition('void => *', [
+      style({transform: 'translateX(-100%)'}),
+      animate(100)
+    ]),
+
+    transition('* => void', [
+      animate(100, style({transform: 'translateX(100%)'}))
+    ])
+    
+  ]),
+
+  ]
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'ToDo List';
+  items = [];
+  newitem="";
+  state: string = 'small'
+
+  animationenter = function(){
+    this.state = 'large'
+  }
+
+  animationleave = function(){
+    this.state = 'small'
+  }
+
+
+  pushitem = function(){
+  	if(this.newitem != ""){
+  		this.items.push(this.newitem);
+  		this.newitem="";
+  	}
+  }
+
+  removeitem = function(index){
+  	this.items.splice(index, 1);
+  }
 }
